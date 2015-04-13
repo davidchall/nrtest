20e3e5039c2ca222c34711beed5b9d1d4f008d91
David Hall <dchall@mgh.harvard.edu>
2015-04-13 12:20:32 -0400

    Improve tests

--- testing/test_execute.py
+++ testing/test_execute.py
@@ -1,7 +1,16 @@
+from nose.tools import assert_almost_equals
+
 from regr_test import execute
 
 
-def test_sleep():
-    cmd = 'sleep 5'
-    p = execute.execute_process(cmd)
+def test_duration():
+    time = 2
+    p = execute.execute_process('sleep %s' % time)
     (duration, data) = execute.monitor_process(p)
+    assert_almost_equals(duration, time, places=1)
+
+
+def test_timeout():
+    p = execute.execute_process('sleep 5')
+    (duration, data) = execute.monitor_process(p, timeout=2)
+    assert duration is None
