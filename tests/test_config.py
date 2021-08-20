def912eb709afb614ab54fcbec3e80a0af1ad015
David Hall <david.hall.physics@gmail.com>
2021-08-19 22:09:57 -0700

    Test AppConfig

---
+++ tests/test_config.py
@@ -0,0 +1,43 @@
+import json
+import pytest
+from pydantic import ValidationError
+
+from nrtest.config import AppConfig, Version
+
+
+app_args = {
+    "name": "app_name",
+    "exe": "app_exe",
+    "version": "1.2.3",
+}
+
+
+def test_create_app_config():
+    c = AppConfig(**app_args)
+    assert c.name == app_args["name"]
+    assert c.exe == app_args["exe"]
+    assert c.version == Version(app_args["version"])
+
+
+def test_validate_app_config():
+    with pytest.raises(ValidationError, match="field required"):
+        AppConfig()
+    with pytest.raises(TypeError, match="multiple values"):
+        AppConfig(**app_args, name="duplicate")
+    with pytest.raises(ValidationError, match="string required"):
+        AppConfig(**dict(app_args, version=1.2))
+    with pytest.raises(ValidationError, match="Invalid version"):
+        AppConfig(**dict(app_args, version="invalid"))
+    with pytest.raises(ValidationError, match="not a valid path"):
+        AppConfig(**app_args, setup_script=123)
+    with pytest.raises(ValidationError, match="not a valid float"):
+        AppConfig(**app_args, timeout="string")
+    with pytest.raises(ValidationError, match="ensure this value is greater than 0"):
+        AppConfig(**app_args, timeout=-5)
+    with pytest.raises(ValidationError):
+        AppConfig(**app_args, unknown="unknown")
+
+
+def test_version_serialization():
+    ser = json.loads(AppConfig(**app_args).json())
+    assert ser["version"] == app_args["version"]
