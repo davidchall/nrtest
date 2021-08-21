cb86c604bbaf76dad3a1f5cafd9d06403c1681dd
David Hall <david.hall.physics@gmail.com>
2021-08-21 10:18:37 -0700

    More tests

--- tests/test_config.py
+++ tests/test_config.py
@@ -2,13 +2,18 @@ import json
 import pytest
 from pydantic import ValidationError
 
-from nrtest.config import AppConfig, Version
+from nrtest.config import AppConfig, TestConfig, Version
 
 
 app_args = {
     "name": "app_name",
     "exe": "app_exe",
     "version": "1.2.3",
+}
+
+test_args = {
+    "name": "test_name",
+    "args": ["arg1", "arg2"],
 }
 
 
@@ -22,6 +27,8 @@ def test_create_app_config():
 def test_validate_app_config():
     with pytest.raises(ValidationError, match="field required"):
         AppConfig()
+    with pytest.raises(ValidationError):
+        AppConfig(**app_args, unknown="unknown")
     with pytest.raises(TypeError, match="multiple values"):
         AppConfig(**app_args, name="duplicate")
     with pytest.raises(ValidationError, match="string required"):
@@ -34,8 +41,33 @@ def test_validate_app_config():
         AppConfig(**app_args, timeout="string")
     with pytest.raises(ValidationError, match="ensure this value is greater than 0"):
         AppConfig(**app_args, timeout=-5)
+
+
+def test_create_test_config():
+    c = TestConfig(**test_args)
+    assert c.name == test_args["name"]
+    assert c.args == test_args["args"]
+
+
+def test_validate_test_config():
+    with pytest.raises(ValidationError, match="field required"):
+        TestConfig()
     with pytest.raises(ValidationError):
-        AppConfig(**app_args, unknown="unknown")
+        TestConfig(**test_args, unknown="unknown")
+    with pytest.raises(TypeError, match="multiple values"):
+        TestConfig(**test_args, name="duplicate")
+    with pytest.raises(ValidationError, match="not a valid list"):
+        TestConfig(**dict(app_args, args="arg1"))
+    with pytest.raises(ValidationError, match="string required"):
+        TestConfig(**test_args, version=1.2)
+    with pytest.raises(ValidationError, match="Invalid version"):
+        TestConfig(**test_args, version="invalid")
+    with pytest.raises(ValidationError, match="Invalid version"):
+        TestConfig(**test_args, minimum_app_version="invalid")
+    with pytest.raises(ValidationError, match="not a valid list"):
+        TestConfig(**test_args, input_files="invalid")
+    with pytest.raises(ValidationError, match="not a valid list"):
+        TestConfig(**test_args, fail_strings="invalid")
 
 
 def test_version_serialization():
